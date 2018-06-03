71f4d92d13870bdcdb414d8b6ab8e4d738ba9636
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2018-06-03 16:16:11 -0300

    Commit da Interface Crud e das implementações dos métodos contidos na InterfaceCrud.

---
+++ src/br/com/framework/implementacao/crud/SimpleJdbcClassImpl.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package br.com.framework.implementacao.crud;
+
+import java.io.Serializable;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author Celina
+ *
+ */
+@Component
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+public class SimpleJdbcClassImpl extends SimpleJdbcCall implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	public SimpleJdbcClassImpl(DataSource dataSource) {
+		super(dataSource);
+	}
+
+}
