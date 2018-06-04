39a25daf7bfbc2d871534a92b978f749fbbf6960
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2018-06-04 09:17:09 -0300

    Classe permissao contendo as permissões para acesso e o utilFramework.

---
+++ src/br/com/framework/utils/UtilFramework.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package br.com.framework.utils;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Celina
+ *
+ */
+@Component
+public class UtilFramework implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private static ThreadLocal<Long> threadLocal = new ThreadLocal<Long>();
+	
+	public synchronized static ThreadLocal<Long> getThreadLocal(){
+		return threadLocal;
+	}
+
+}
