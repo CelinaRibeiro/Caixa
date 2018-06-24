9955d54e77419f9e45c3c335e92584b1ad6dbb44
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2018-06-24 11:01:27 -0300

    Finalizando template.

--- WebContent/resources/javascript/scripty.js
+++ WebContent/resources/javascript/scripty.js
@@ -1,3 +1,44 @@
+var arrayIdsElementsPage = new Array;
+
+
+/**
+ * Carrega um array global com os ids de todos os componentes da pagina Para ter
+ * faciliades em obtencao de valores dos componentes bem como trabalhar com ajax
+ */
+function carregarIdElementosPagina() {
+	 arrayIdsElementsPage = new Array;
+	 for (form = 0 ; form <= document.forms.length; form++){
+		 var formAtual = document.forms[form];
+		 if (formAtual != undefined) {
+			 for (i = 0; i< document.forms[form].elements.length; i++){
+				 if(document.forms[form].elements[i].id != '') {
+					 arrayIdsElementsPage[i] = document.forms[form].elements[i].id;
+				 }
+			 }	
+		 }
+	 }
+}
+
+
+
+/**
+ * Retorno o valor do id do componente dentro do documento html passando como
+ * parametro a descrição do id declarada em jsf
+ * 
+ * @param id
+ */
+function getValorElementPorId(id) {
+	 carregarIdElementosPagina();
+	 for (i = 0; i< arrayIdsElementsPage.length; i++){
+		 var valor =  ""+arrayIdsElementsPage[i];
+		 if (valor.indexOf(id) > -1) {
+			return valor;
+	}
+  }	
+	 return idundefined;
+}
+
+
 
 function logout(contextPath) {
 	var post = 'invalidar_session';
@@ -54,3 +95,61 @@ function redirecionarPagina(context, pagina){
 	document.location = context + pagina;
 	
 }
+
+
+function localeData_pt_br() {
+	PrimeFaces.locales['pt'] = {
+		closeText : 'Fechar',
+		prevText : 'Anterior',
+		nextText : 'Próximo',
+		currentText : 'Começo',
+		monthNames : [ 'Janeiro', 'Fevereiro', 'Marcio', 'Abril', 'Maio',
+				'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro',
+				'Dezembro' ],
+		monthNamesShort : [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul',
+				'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+		dayNames : [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta',
+				'Sexta', 'Sábado' ],
+		dayNamesShort : [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb' ],
+		dayNamesMin : [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+		weekHeader : 'Semana',
+		firstDay : 0,
+		isRTL : false,
+		showMonthAfterYear : false,
+		yearSuffix : '',
+		timeOnlyTitle : 'São Horas',
+		timeText : 'Tempo',
+		hourText : 'Hora',
+		minuteText : 'Minuto',
+		secondText : 'Segundo',
+		ampm : false,
+		month : 'Mês',
+		week : 'Semana',
+		day : 'Dia',
+		allDayText : 'Todo o Dia'
+	};
+}
+
+
+function initTamplete() {
+	$(document).ready(function() {
+		  $('#menupop').hide();
+		  $('#barramenu').hide();
+		  $('#barramenu').css("left", "-200px");
+		  $('#iniciarmenu').click(function() {
+		  	if ($('#barramenu').is(':visible')) {
+		  	  ocultarMenu();
+		  	} else {
+		  	  $('#barramenu').show();
+		  	  $('#barramenu').animate({"left":"0px"}, "slow");	
+		  	}
+		  });
+		});
+	}
+
+function ocultarMenu(){
+	$('#barramenu').animate({"left": "-200px"}, "slow", function(){
+		$('#barramenu').hide();
+	});
+}
+
