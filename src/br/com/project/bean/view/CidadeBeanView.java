e3914c03a44d1e3a639a012a990d2bdce485a556
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2018-06-25 08:58:38 -0300

    Cad cidade.

--- src/br/com/project/bean/view/CidadeBeanView.java
+++ src/br/com/project/bean/view/CidadeBeanView.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 
 import br.com.project.bean.geral.BeanManagedViewAbstract;
 import br.com.project.geral.controller.CidadeController;
+import br.com.project.model.classes.Cidade;
 
 /**
  * @author Celina
@@ -24,8 +25,25 @@ public class CidadeBeanView extends BeanManagedViewAbstract {
 
 	private static final long serialVersionUID = 1L;
 	
+	private Cidade objetoSelecionado = new Cidade();
+	
 	@Autowired
 	private CidadeController cidadeController;
+	
+	@Override
+	public String save() throws Exception {
+		System.out.println(objetoSelecionado.getCid_descricao());
+		return "";
+	}
+
+	public Cidade getObjetoSelecionado() {
+		return objetoSelecionado;
+	}
+
+	public void setObjetoSelecionado(Cidade objetoSelecionado) {
+		this.objetoSelecionado = objetoSelecionado;
+	}
+	
 	
 	
 
